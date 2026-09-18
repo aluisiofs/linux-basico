120e370f8def5b0877fd17a1b8ec422575aef6a3
Aluisio Felipe Silva <aluisiofelipedasilva1@gmail.com>
2026-09-18 08:56:12 -0300

    Cria arquivo "verificar-sistema.sh" para estudo.

---
+++ scripts/verificar-sistema.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+echo "==========================================" 
+echo " VERIFICAÇÃO BÁSICA DO SISTEMA " 
+echo "==========================================" 
+echo "Data e Hora atual: $(date)" 
+echo "Usuário logado: $(whoami)" 
+echo "Diretório atual: $(pwd)" 
+echo "=========================================="
